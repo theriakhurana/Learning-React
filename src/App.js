f8797292e28599aff4df1eabf5fdff322ad5370b
theriakhurana <theriakhurana@gmail.com>
2025-03-03 14:34:12 +0530

    score keeper implementation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css"
 // import Counter from "./Counter";
 // import Dumbo from "./Dumbo";
-// import ScoreKeeper from "./ScoreKeeper";
-import EmojiClicker from "./EmojiClicker";
+import ScoreKeeper from "./ScoreKeeper";
+// import EmojiClicker from "./EmojiClicker";
 function App(){
   return (
     <div className="App">
@@ -11,7 +11,7 @@ function App(){
       {/* <ScoreKeeper /> */}
       {/* <EmojiClicker /> */}
 
-      <ScoreKeeper numPlayers {3} target={3} />
+      <ScoreKeeper numPlayers={3} target={3} />
     </div>
   );
 }
